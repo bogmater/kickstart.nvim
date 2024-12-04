d83947e0a9cf9b163a39dc1ab83709d90a98b7a4
bogmater <dominik.trupcevic@gmail.com>
2024-12-04 20:43:12 +0100

    updates

--- lua/custom/plugins/catpuccin.lua
+++ lua/custom/plugins/catpuccin.lua
@@ -1,11 +1,11 @@
 return {
-  'ronisbr/nano-theme.nvim',
+  'catppuccin/nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     -- Load the colorscheme here
-    vim.cmd.colorscheme 'nano-theme'
-    vim.cmd 'set background=light'
+    vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.cmd 'set background=dark'
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
