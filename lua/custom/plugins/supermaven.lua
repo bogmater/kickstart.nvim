22c7825db6839958c96ce4a25d68441ecc300106
bogmater <dominik.trupcevic@gmail.com>
2025-03-21 21:46:03 +0100

    adjust supermaven bindings

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,6 +1,12 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup {}
+    require('supermaven-nvim').setup {
+      keymaps = {
+        accept_suggestion = '<C-k>',
+        clear_suggestion = '<C-S-Tab>',
+        accept_word = '<C-j>',
+      },
+    }
   end,
 }
